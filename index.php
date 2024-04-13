93d21895ac0f5b2d9c366c46c125648c12d6bd33
chiloanerk <chiloanerk@gmail.com>
2024-04-13 21:43:19 +0200

    change a few

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-
-// Include the index.php file from the public folder
-require_once __DIR__ . '/public/index.php';
--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 
 use App\controllers\Router;
 
-require_once __DIR__ . '/../app/helpers/functions.php';
+require_once '../app/helpers/functions.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
